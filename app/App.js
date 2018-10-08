d965a72aef43f4f0a5973715424982e25fd28bab
jillyan <zyan@talend.com>
2018-10-08 22:12:18 +0800

    add days without its own logic

--- app/App.js
+++ app/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Grid, Row, Col, Image, Thumbnail} from 'react-bootstrap';
+import {Grid, Row, Col} from 'react-bootstrap';
 import {hot} from 'react-hot-loader'
 import Counter from './Counter';
 import './jsj.css';
@@ -51,76 +51,12 @@ class StuffList extends React.Component {
     render() {
         return (
             <Grid>
+                <Counter min='0' max='100'price={this.state.unitPrice} image="app/picture/banzi.jpg" num={this.state.unitNum}  changeNum={num => this.changeUnitNum(num)}/>
+                <Counter min='0' max='100'price={this.state.banziPrice} image="app/picture/banzi.jpg" num={this.state.banziNum}  changeNum={num => this.changeBanziNum(num)}/>
+                <Counter min='0' max='100'price={this.state.jietouPrice} image="app/picture/jietou.jpg" num={this.state.jietouNum}  changeNum={num => this.changeJietouNum(num)}/>
+                <Counter min='0' max='100'price={this.state.lunziPrice} image="app/picture/lunzi.jpg" num={this.state.lunziNum}  changeNum={num => this.changeLunziNum(num)}/>
                 <Row className="show-grid">
-                    <Col xs={2} md={2}>
-                        <Thumbnail href="#" alt="板子" src="app/picture/banzi.jpg"/>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common"> {this.state.unitPrice}元/每天每套</div>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common">
-                            <Counter min='0' max='100' num={this.state.unitNum}
-                                     changeNum={num => this.changeUnitNum(num)}/>
-                        </div>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common"> 总价{this.state.unitPrice * this.state.unitNum}元</div>
-                    </Col>
-                </Row>
-                <Row className="show-grid">
-                    <Col xs={2} md={2}>
-                        <Thumbnail href="#" alt="板子" src="app/picture/banzi.jpg"/>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common"> {this.state.banziPrice}元/每天</div>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common">
-                            <Counter min='0' max='100' num={this.state.banziNum}
-                                     changeNum={num => this.changeBanziNum(num)}/>
-                        </div>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common"> 总价{this.state.banziPrice * this.state.banziNum}元</div>
-                    </Col>
-                </Row>
-                <Row className="show-grid">
-                    <Col xs={2} md={2}>
-                        <Thumbnail href="#" alt="接头" src="app/picture/jietou.jpg"/>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common"> {this.state.jietouPrice}元/每天</div>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common">
-                            <Counter min='0' max='100' num={this.state.jietouNum}
-                                     changeNum={num => this.changeJietouNum(num)}/>
-                        </div>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common"> 总价{this.state.jietouPrice * this.state.jietouNum}元</div>
-                    </Col>
-                </Row>
-                <Row className="show-grid">
-                    <Col xs={2} md={2}>
-                        <Thumbnail href="#" alt="轮子" src="app/picture/lunzi.jpg"/>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common"> {this.state.lunziPrice}元/每天</div>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common">
-                            <Counter min='0' max='100' num={this.state.lunziNum}
-                                     changeNum={num => this.changeLunziNum(num)}/>
-                        </div>
-                    </Col>
-                    <Col xs={2} md={2}>
-                        <div className="common"> 总价{this.state.lunziPrice * this.state.lunziNum}元</div>
-                    </Col>
-                </Row>
-                <Row className="show-grid">
-                    <Col xs={6} md={6}>
+                    <Col xs={8} md={8}>
                     </Col>
                     <Col xs={2} md={2}>
                         总计{this.getTotal()}元
--- app/Counter.js
+++ app/Counter.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Row, Col, Thumbnail} from 'react-bootstrap';
 
 class Counter extends React.Component {
     constructor(props) {
@@ -26,11 +27,33 @@ class Counter extends React.Component {
     }
 
     render(){
-        return (<div>套数：
-            <input type="button" value='-' onClick={this.decrease.bind(this)}/>
-            <input type="text" size="5" value={this.state.num} onChange={e => this.handlerChange(e)}/>
-            <input type="button" value='+' onClick={this.increase.bind(this)}/>
-        </div>)
+        return (<Row className="show-grid">
+            <Col xs={2} md={2}>
+                <Thumbnail href="#" alt="轮子" src={this.props.image}/>
+            </Col>
+            <Col xs={2} md={2}>
+                <div className="common"> {this.props.price}元/每天</div>
+            </Col>
+            <Col xs={2} md={2}>
+                <div className="common">
+                    套数：
+                        <input type="button" value='-' onClick={this.decrease.bind(this)}/>
+                        <input type="text" size="5" value={this.state.num} onChange={e => this.handlerChange(e)}/>
+                        <input type="button" value='+' onClick={this.increase.bind(this)}/>
+                </div>
+            </Col>
+            <Col xs={2} md={2}>
+                <div className="common">
+                    天数：
+                    <input type="button" value='-' onClick={this.decrease.bind(this)}/>
+                    <input type="text" size="5" value={this.state.num} onChange={e => this.handlerChange(e)}/>
+                    <input type="button" value='+' onClick={this.increase.bind(this)}/>
+                </div>
+            </Col>
+            <Col xs={2} md={2}>
+                <div className="common"> 总价{this.props.price * this.state.num}元</div>
+            </Col>
+        </Row>);
     }
 }
 
